37d107d0c47e7436a73da74f887d1eba972694eb
NiceBruce <demin.b.d@gmail.com>
2023-04-26 16:21:24 +0500

    the step is done

--- src/test/java/hexlet/code/AppApplicationTests.java
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -2,9 +2,14 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class AppApplicationTests {
+    @Test
+    void testInit() {
+        assertThat(true).isEqualTo(true);
+    }
 @Test
 void contextLoads() {
 }
